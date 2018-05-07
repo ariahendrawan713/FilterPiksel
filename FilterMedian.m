434f257b201a88f43a70721e4326b66a8746a03c
ariahendrawan713 <38807873+ariahendrawan713@users.noreply.github.com>
2018-05-07 14:48:10 +0700

    Penambahan Subplot

--- FilterMedian.m
+++ FilterMedian.m
@@ -1,29 +1,30 @@
-F = imread('C:\Image\mobil.png');
+A = imread('1.png');
+F=rgb2gray(A);
 [tinggi, lebar] = size(F);
 for baris=2 : tinggi-1
-for kolom=2 : lebar-1
-data = [F(baris-1, kolom-1) ...
-F(baris-1, kolom) ...
-F(baris-1, kolom+1) ...
-F(baris, kolom-1) ...
-F(baris, kolom) ...
-F(baris, kolom+1) ...
-F(baris+1, kolom-1) ...
-F(baris+1, kolom) ...
-F(baris+1, kolom+1)];
-% Urutkan
-for i=1 : 8
-for j=i+1 : 9
-if data(i) > data(j)
-tmp = data(i);
-data(i) = data(j);
-data(j) = tmp;
-end
-end
-end
+    for kolom=2 : lebar-1
+        data = [F(baris-1, kolom-1) ...
+            F(baris-1, kolom) ...
+            F(baris-1, kolom+1) ...
+            F(baris, kolom-1) ...
+            F(baris, kolom) ...
+            F(baris, kolom+1) ...
+            F(baris+1, kolom-1) ...
+            F(baris+1, kolom) ...
+            F(baris+1, kolom+1)];
+        for i=1 : 8
+            for j=i+1 : 9
+                if data(i) > data(j)
+                    tmp = data(i);
+                    data(i) = data(j);
+                    data(j) = tmp;
+                end
+            end
+        end
 % Ambil nilai median
 G(baris, kolom) = data(5);
+    end
 end
-end
-figure(1); imshow(G);
-clear
+subplot(2,2,1);imshow(F);subplot(2,2,2);imhist(F);
+subplot(2,2,3);imshow(G);subplot(2,2,4);imhist(G);
+clear;
